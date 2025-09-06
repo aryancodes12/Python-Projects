29d349ed11d62092332abb44911bdc746948c0d3
aryancodes12 <9040aryangupta@gmail.com>
2025-09-06 09:43:39 +0530

    Making functions consistent

--- todo/todo.py
+++ todo/todo.py
@@ -9,35 +9,35 @@ def show_tasks(tasks):
         for i, task in enumerate(tasks, 1):
             print(f"\t\033[35m{i}. {task}\033[0m")
 
-def remove_tasks(functions):
-        if functions.lower() in ["1","remove"]:
-            task_remove = input("Enter Task's name to remove it: ").title()
-            try:
-                tasks.remove(task_remove)
-                print(f"\n{task_remove.title()} removed from the tasks\n")
+def remove_task():
+    task_remove = input("Enter Task's name to remove it: ").title()
+    try:
+        tasks.remove(task_remove)
+        print(f"\n{task_remove.title()} removed from the tasks\n")
                 # show_tasks(tasks)
 
-            except ValueError:
-                print(f"\n\033[1;91mTask doesn't exist in the Tasks.\033[0m")
+    except ValueError:
+        print(f"\n\033[1;91mTask doesn't exist in the Tasks.\033[0m")
                 # show_tasks(tasks)
-        print("\n\033[1;92mTasks Updated\033[0m\n")
+    print("\n\033[1;92mTasks Updated\033[0m\n")
 
-def  reverse_tasks(functions):
-    if functions.lower() in ["4","reverse"]:
-        tasks.reverse()
-        print("\n\033[1;34mTask in reverse:\033[0m")
-        show_tasks(tasks)
-        print("\nTasks are reversed.")
+def  reverse_tasks():
+    tasks.reverse()
+    print("\n\033[1;34mTask in reverse:\033[0m")
+    show_tasks(tasks)
+    print("\nTasks are reversed.")
 
-def sort_tasks(functions):
-    if functions.lower() in ["3", "sort"]:
-        tasks.sort()
-        print(f"\nTask have been sorted:")
-        show_tasks(tasks)
+def sort_tasks():
+    tasks.sort()
+    show_tasks(tasks)
 
-def clear_tasks(functions):
-    tasks.clear()
-    print("\n\033[1;91mAll tasks are cleared.\033[0m")
+def clear_tasks():
+    confirmation = input("Do you really want to clear everything (y or n)? ").lower().strip()
+    if confirmation == 'y':
+        tasks.clear()
+        print("\n\033[1;91mAll tasks are cleared.\033[0m")
+    else:
+        print("\n\033[91mTasks are not clear yet.\033[0m")
 
 print("\n\033[1;92m------TO-DO List app------\033[0m\n")
 print("\n\033[1;92m------Add Task------\033[0m\n")
@@ -66,16 +66,16 @@ while True:
     print('''\033[1;93m1. Remove task.           2. Show task.\n3. Sort 'A to Z'.         4. Reverse task.\n5. Clear task.            6. Add tasks.\033[0m''')
     functions = input("\nEnter function's number or name (or \033[1;94m'q'\033[0m to exit): ")
     if functions.lower() in ["1","remove"]:
-        remove_tasks(functions)
+        remove_task()
     elif functions.lower() in ["2", "show"]:
         show_tasks(tasks)
         print("\n")
     elif functions.lower() in ["3", "sort"]:
-        sort_tasks(functions)
+        sort_tasks()
     elif functions.lower() in ["4", "reverse"]:
-        reverse_tasks(functions)
+        reverse_tasks()
     elif functions.lower() in ["5", "clear"]:
-        clear_tasks(functions)
+        clear_tasks()
     elif functions.lower() in ["6", "add tasks"]:
         print("\nAdd tasks:")
         main()
